75c8e94d21fca81d5adf5da29fe337cab521620a
BavoJanssensTIN <bavo.janssens@student.hogent.be>
2018-09-27 12:19:06 +0200

    Creëer de klasse BankAccount.

---
+++ Banking2C1/Models/Domain/BankAccount.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Banking2C1.Models.Domain
+{
+    //als geen visibiliteit gedeclareerd -> default: internal (enkel zichtbaar binnen assembly .dll)
+    public class BankAccount
+    {
+        #region Fields
+        public const decimal WithdrawCost = 0.10M;
+        private decimal _balance;
+        #endregion
+
+        #region Properties
+        public string AccountNumber { get; set; }
+
+        public decimal Balance
+        {
+            get { return _balance; }
+            private set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Invalid value for balance");
+                }
+                _balance = value;
+            }
+        } 
+        #endregion
+
+        #region Constructors
+        public BankAccount(string accountNumber)
+        {
+            AccountNumber = accountNumber;
+        }
+
+        public BankAccount(string accountNumber, decimal balance) : this(accountNumber)
+        {
+            Balance = balance;
+        }
+        #endregion
+
+        #region Methods
+        public void Deposit(decimal amount)
+        {
+            Balance += amount;
+        }
+
+        public void Withdraw(decimal amount)
+        {
+            Balance -= amount + WithdrawCost;
+        } 
+        #endregion
+
+
+    }
+}
---
+++ Banking2C1/Program.cs
@@ -0,0 +1,26 @@
+﻿using Banking2C1.Models.Domain;
+using System;
+
+namespace Banking2C1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount myBA = new BankAccount("123-123123123-23");
+            BankAccount anotherBA = new BankAccount("456-456456456-99", 1000);
+
+            Console.WriteLine($"The withdrawcost is {BankAccount.WithdrawCost} Euro");
+            Console.WriteLine($"Balance is currently {anotherBA.Balance} Euro");
+
+            Console.WriteLine($"Balance is currently {myBA.Balance} Euro");
+            myBA.Deposit(100);
+            Console.WriteLine($"Balance is currently {myBA.Balance} Euro");
+            myBA.Withdraw(50);
+            Console.WriteLine($"Balance is currently {myBA.Balance} Euro");
+            myBA.Withdraw(20);
+            Console.WriteLine($"Balance is currently {myBA.Balance} Euro");
+            Console.ReadLine();
+        }
+    }
+}
